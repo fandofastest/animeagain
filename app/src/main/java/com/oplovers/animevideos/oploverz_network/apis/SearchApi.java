e85cd2edaac7155b74639d2b837d6beb97a4c3cc
fando <fandofast@gmail.com>
2020-01-23 17:18:33 +0700

    Initial commit

---
+++ app/src/main/java/com/oplovers/animevideos/oploverz_network/apis/SearchApi.java
@@ -0,0 +1,15 @@
+package com.oplovers.animevideos.oploverz_network.apis;
+
+import com.oplovers.animevideos.oploverz_network.model.SearchModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface SearchApi {
+
+    @GET("search")
+    Call<SearchModel> getSearchData(@Query("api_secret_key") String apiSecreteKey,
+                                    @Query("q") String query);
+
+}
